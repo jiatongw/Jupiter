cabe19adf45852ae8e429630bb6cb99cf0753049
Quynh <quynhngu@usc.edu>
2018-03-27 18:16:24 -0700

    modify exec

--- scripts/build_push_base_rpis2.py
+++ scripts/build_push_base_rpis2.py
@@ -15,8 +15,8 @@ def build_push_rpis_circe():
 
 def build_push_rpis_exec():
 	os.chdir(jupiter_config.EXEC_PROFILER_PATH )
-	os.system("sudo docker build -f rpi_exec_home.Dockerfile ../.. -t docker.io/anrg/rpi_exec_home:v0")
-	os.system("sudo docker push docker.io/anrg/rpi_exec_home:v0")
+	#os.system("sudo docker build -f rpi_exec_home.Dockerfile ../.. -t docker.io/anrg/rpi_exec_home:v0")
+	#os.system("sudo docker push docker.io/anrg/rpi_exec_home:v0")
 
 	os.system("sudo docker build -f rpi_exec_worker.Dockerfile ../.. -t docker.io/anrg/rpi_exec_worker:v0")
 	os.system("sudo docker push docker.io/anrg/rpi_exec_worker:v0")
@@ -28,11 +28,11 @@ def build_push_rpis_heft():
 
 def build_push_rpis_netr():
 	os.chdir(jupiter_config.NETR_PROFILER_PATH)
-	os.system("sudo docker build -f rpi_profiler_home.Dockerfile ../.. -t docker.io/anrg/rpi_netr_home:v0")
-	os.system("sudo docker push docker.io/anrg/rpi_netr_home:v0")
+	#os.system("sudo docker build -f rpi_profiler_home.Dockerfile ../.. -t docker.io/anrg/rpi_netr_home:v0")
+	#os.system("sudo docker push docker.io/anrg/rpi_netr_home:v0")
 
-	#os.system("sudo docker build -f rpi_profiler_worker.Dockerfile ../.. -t docker.io/anrg/rpi_netr_worker:v0")
-	#os.system("sudo docker push docker.io/anrg/rpi_netr_worker:v0")
+	os.system("sudo docker build -f rpi_profiler_worker.Dockerfile ../.. -t docker.io/anrg/rpi_netr_worker:v0")
+	os.system("sudo docker push docker.io/anrg/rpi_netr_worker:v0")
 
 def build_push_rpis_wave():
 	os.chdir(jupiter_config.WAVE_PATH)
